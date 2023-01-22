48a0d96b4c657bd794c4818eaa61d8d9190ae4f4
Zacster2005 <zacster2005@gmail.com>
2023-01-22 16:40:00 -0500

    Clicksfx

--- AbyssExperiment/Assets/Scripts/Menu.cs
+++ AbyssExperiment/Assets/Scripts/Menu.cs
@@ -7,18 +7,24 @@ public class Menu : MonoBehaviour
     public string firstLevel;
     public GameObject optionsScreen;
 
+    //Sounds
+    public AudioSource Onclicksfx;
+
    public void Play()
     {
+        Onclicksfx.Play();
         SceneManager.LoadScene(firstLevel);
     }
 
     public void OpenOptions()
     {
+        Onclicksfx.Play();
         optionsScreen.SetActive(true);
     }
 
     public void CloseOptions()
     {
+        Onclicksfx.Play();
         optionsScreen.SetActive(false);
     }
 
