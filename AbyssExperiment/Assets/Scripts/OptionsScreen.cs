354ff439022e86a19edb8176e130ebc2973d350e
FlamboQc <boisjolyelliot@gmail.com>
2023-01-23 00:23:11 -0500

    ResolutionButtons

--- AbyssExperiment/Assets/Scripts/OptionsScreen.cs
+++ AbyssExperiment/Assets/Scripts/OptionsScreen.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
     public Toggle fullscreenTog, vsyncTog;
+    public List<ResItem> resolutions= new List<ResItem>();
+    private int selectedResolution;
+    public TMP_Text resolutionLabel;
 
     private void Start()
     {
@@ -20,6 +24,34 @@ public class Options : MonoBehaviour
             vsyncTog.isOn = true;
         }
     }
+
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if(selectedResolution < 0 )
+        {
+            selectedResolution = 0;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
     public void ApplyGraphics()
     {
         Screen.fullScreen = fullscreenTog.isOn;
@@ -32,4 +64,10 @@ public class Options : MonoBehaviour
             QualitySettings.vSyncCount = 0;
         }
     }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
