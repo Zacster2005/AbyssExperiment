e0c7802ca328a3b0768936e501a83a1d03fe0b5d
Zacster2005 <zacster2005@gmail.com>
2023-05-07 17:32:35 -0400

    CamShake

--- AbyssExperiment/Assets/Scripts/LightShake.cs
+++ AbyssExperiment/Assets/Scripts/LightShake.cs
@@ -4,29 +4,48 @@ using UnityEngine;
 
 public class LightShake : MonoBehaviour 
 {
+    public GameObject Cam;
 
+    public bool IsShake = false;
 
 
     public IEnumerator Shake(float duration, float magnitude)
     {
-        Vector3 originalPos = transform.localPosition;
-
-        float elapsed = 0.0f;
-
-        while (elapsed < duration)
+        if(!IsShake)
         {
-           
-            float y = Random.Range(-.25f, .25f);
+            IsShake = true;
 
-            transform.localPosition = new Vector3(originalPos.x, y, originalPos.z);
+            Vector3 originalPos = transform.localPosition;
 
-            elapsed += Time.deltaTime;
+            Vector3 CamPos = Cam.transform.localPosition;   
 
-            yield return null;
-        }
-    
+
+            float elapsed = 0.0f;
+
+            while (elapsed < duration)
+            {
+
+                float y = Random.Range(-.25f, .25f);
+
+                transform.localPosition = new Vector3(originalPos.x, y, originalPos.z);
+
+                float Camy = Random.Range(0.9f, 1.1f);
+
+                Cam.transform.localPosition = new Vector3(CamPos.x, Camy, CamPos.z);
+
+                elapsed += Time.deltaTime;
+
+                yield return null;
+
+                IsShake = false;
+            }
+
             transform.localPosition = originalPos;
 
+            Cam.transform.localPosition = new Vector3(CamPos.x, 1, CamPos.z);
+        }
+        
+        
     }
 
 
