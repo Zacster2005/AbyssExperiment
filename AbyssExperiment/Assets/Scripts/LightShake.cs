1cf28b00f6d7615563d8eb00cc0551df935de745
Zacster2005 <zacster2005@gmail.com>
2023-05-20 15:30:40 -0400

    texture and shake

--- AbyssExperiment/Assets/Scripts/LightShake.cs
+++ AbyssExperiment/Assets/Scripts/LightShake.cs
@@ -17,7 +17,7 @@ public class LightShake : MonoBehaviour
 
             Vector3 originalPos = transform.localPosition;
 
-            Vector3 CamPos = Cam.transform.localPosition;   
+            
 
 
             float elapsed = 0.0f;
@@ -29,20 +29,13 @@ public class LightShake : MonoBehaviour
 
                 transform.localPosition = new Vector3(originalPos.x, y, originalPos.z);
 
-                float Camy = Random.Range(0.95f, 1.05f);
-
-                Cam.transform.localPosition = new Vector3(CamPos.x, Camy, CamPos.z);
-
-                elapsed += Time.deltaTime;
-
                 yield return null;
 
                 IsShake = false;
             }
 
             transform.localPosition = originalPos;
-
-            Cam.transform.localPosition = new Vector3(CamPos.x, 1, CamPos.z);
+         
         }
         
         
